a39935c69ee0d8040c760810ce2635cbd851d142
Sean889 <sean@lynches.ca>
2015-06-02 22:09:54 -0400

    Revert "Revert "Adde planet implementation""
    This reverts commit edb4ab423a5ba3289cc2ba29341cc9f51b973a55.

---
+++ LodPlanet/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace LodPlanet
+{
+    public class Camera
+    {
+        public double NearZ, FarZ;
+        public double FovY;
+        public double Aspect;
+
+        public Vector3d Position;
+        public Quaterniond Rotation;
+    }
+}
--- ThreadPool/Future.cs
+++ ThreadPool/Future.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ThreadPool
+namespace CppThreadPool
 {
     //Generic type
     public struct Future<T>
